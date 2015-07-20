af7b922e091bafe66d1dec0986ad39863efc06b8
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-07-20 16:03:40 -0500

    Cleaned up code

--- lib/puppet/reports/smtp.rb
+++ lib/puppet/reports/smtp.rb
@@ -2,40 +2,54 @@ require 'puppet'
 require 'yaml'
 require 'net/smtp'
 
+unless Puppet.version >= '3.0.0'
+  fail "This report processor requires Puppet version 3.0.0 or later"
+end
+
 Puppet::Reports.register_report(:smtp) do
-  configfile = File.join([File.dirname(Puppet.settings[:config]),
-                          'smtpreports.yaml'])
-  raise(Puppet::ParseError, "SMTP report config file #{configfile} does not exist!") unless File.exist?(configfile)
-  SMTP_CONFIG = YAML.load_file(configfile)
+  configfile = File.join([File.dirname(Puppet.settings[:config]), 'smtpreports.yaml'])
+  unless File.exist?(configfile)
+    raise(Puppet::ParseError, "SMTP report config file #{configfile} missing or not readable!")
+  end
+  CONFIG = YAML.load_file(configfile)
 
   desc <<-DESC
   Send report information to recipients via SMTP.
   DESC
 
   def process
-    if status == 'failed'
-      Puppet.debug "Sending status for #{host} via SMTP"
+    case status
+    when 'failed'
+      info = 'failed!'
+    when 'changed'
+      info = 'had changes.'
+    else
+      return true
+    end
 
-      if SMTP_CONFIG[:smtp_auth_type]
-        SMTP_CONFIG[:smtp_auth_type] = SMTP_CONFIG[:smtp_auth_type].to_sym
-      end
+    if CONFIG[:smtp_auth_type]
+      CONFIG[:smtp_auth_type] = CONFIG[:smtp_auth_type].to_sym
+    end
 
-      smtp_con = Net::SMTP.new(SMTP_CONFIG[:smtp_server],
-                               SMTP_CONFIG[:smtp_port])
-      smtp_con.start('localhost',
-                     SMTP_CONFIG[:smtp_account],
-                     SMTP_CONFIG[:smtp_secret],
-                     SMTP_CONFIG[:smtp_auth_type])
+    msg_str = "From: #{CONFIG[:smtp_from]}\n"
+    msg_str += "To: #{CONFIG[:smtp_to]}\n"
+    msg_str += "Subject: Puppet catalog run on #{host} #{info}\n\n"
+    msg_str += "The catalog run on #{host}, started at #{time}, #{info}\n"
 
-      msg_str = "From: #{SMTP_CONFIG[:smtp_from]}\n"
-      msg_str += "To: #{SMTP_CONFIG[:smtp_to]}\n"
-      msg_str += "Subject: #{host} run failed!\n\n"
-      msg_str += "The catalog run for #{host}, started at #{time}, failed!\n"
+    begin
+      smtp_con = Net::SMTP.new(CONFIG[:smtp_server],
+                               CONFIG[:smtp_port])
+      smtp_con.start(CONFIG[:smtp_helo],
+                     CONFIG[:smtp_account],
+                     CONFIG[:smtp_secret],
+                     CONFIG[:smtp_auth_type])
 
       smtp_con.send_message(msg_str,
-                            SMTP_CONFIG[:smtp_from],
-                            SMTP_CONFIG[:smtp_to])
+                            CONFIG[:smtp_from],
+                            CONFIG[:smtp_to].split(","))
       smtp_con.finish
+    rescue Exception => e
+      Puppet.err("Failed to send SMTP report: #{e.message}")
     end
   end
 end
