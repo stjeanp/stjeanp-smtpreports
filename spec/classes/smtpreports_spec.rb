3a93fe1665313aa96c9a7f6b5c6b3a831323cdd7
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-07-31 14:39:59 -0500

    Updated tests to better handle PE and OSS version differences

--- spec/classes/smtpreports_spec.rb
+++ spec/classes/smtpreports_spec.rb
@@ -1,31 +1,36 @@
 require 'spec_helper'
 
 describe 'smtpreports', :type => :class do
-  context 'default' do
-    let(:facts) { { :puppetversion => '3.7.5'} }
+  after :each do
+    Facter.clear
+    Facter.clear_messages
+  end
+
+  context 'puppet oss 3.7.5' do
+    let(:facts) { { :puppetversion => '3.7.5', :is_pe => false } }
     it { should contain_class('smtpreports') }
     it { should contain_class('smtpreports::params') }
-    it { should contain_file('smtpreports-yaml-config').with(:owner => 'puppet') }
+    it { should contain_file('smtpreports-yaml-config').with(:owner => 'root') }
   end
 
   context 'pe, pre-2015.2' do
-    let(:facts) { { :is_pe => true } }
+    let(:facts) { { :puppetversion => '3.8.1', :is_pe => true } }
     it { should contain_class('smtpreports') }
     it { should contain_class('smtpreports::params') }
-    it { should contain_file('smtpreports-yaml-config').with(:owner => 'pe-puppet') }
+    it { should contain_file('smtpreports-yaml-config').with(:owner => 'root') }
   end
 
-  context 'pe, >=2015.2' do
-    let(:facts) { { :pe_server_version => "2015.2.0" } }
+  context 'pe, >=2015.2, puppet master' do
+    let(:facts) { { :puppetversion => '4.2.1', :pe_server_version => "2015.2.0", :is_pe => false } }
     it { should contain_class('smtpreports') }
     it { should contain_class('smtpreports::params') }
-    it { should contain_file('smtpreports-yaml-config').with(:owner => 'pe-puppet') }
+    it { should contain_file('smtpreports-yaml-config').with(:owner => 'root') }
   end
 
-  context 'puppet 4' do
-    let(:facts) { { :puppetversion => '4.0.0', :is_pe => false } }
+  context 'puppet 4.2.1 AIO agent' do
+    let(:facts) { { :puppetversion => '4.2.1', :is_pe => false } }
     it { should contain_class('smtpreports') }
     it { should contain_class('smtpreports::params') }
-    it { should contain_file('smtpreports-yaml-config').with(:owner => 'puppet') }
+    it { should contain_file('smtpreports-yaml-config').with(:owner => 'root') }
   end
 end
